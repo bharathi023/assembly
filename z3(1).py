1fbc0e9e0d81a0285e3b30647831604088a6ece8
Putta23 <31534695+bharathi023@users.noreply.github.com>
2018-03-22 10:17:58 +0530

    Add files via upload

---
+++ z3(1).py
@@ -0,0 +1,6 @@
+from z3 import *
+x = Real('x')
+s = Solver()
+s.add(x*x - 2*x + 1==0)
+s.check()
+print s.model()
---
+++ z3(3).py
@@ -0,0 +1,10 @@
+from z3 import *
+x = Real('x')
+y = Real('y')
+z = Real('z')
+s = Solver()
+s.add(3*x + 2*y - z==1)
+s.add(2*x - 2*y + 4*z==-2)
+s.add(-x +(1/2)*y -z==0)
+s.check()
+print s.model()
